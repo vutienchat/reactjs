25840832711e95b41624c652a66e9eba97490b69
Your Name <you@example.com>
2021-07-15 22:08:04 +0700

    edit userApi

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,26 +1,25 @@
-import { axiosClient } from './axiosClient';
-import axios from 'axios';
+import { axiosClient } from "./axiosClient";
+import axios from "axios";
 const UserAPI = {
-    login(data) {
-        const url = `/signin`;
-        return axiosClient.post(url,data);
-    },
-    add(data) {
-        const url = `/signup`;
-        return axiosClient.post(url,data);
-    },
-    signOut() {
-        const url = `/signout`;
-        return axiosClient.get(url);
-    },
-    profile(token,id) {
-        const url = `http://localhost:4000/api/secret/${id}`
-        return axios.get(url,{
-            headers:{
-                authorization: `Bearer ${token}`
-            }
-        })
-    }
-
-}
+  login(data) {
+    const url = `/signin`;
+    return axiosClient.post(url, data);
+  },
+  add(data) {
+    const url = `/signup`;
+    return axiosClient.post(url, data);
+  },
+  signOut() {
+    const url = `/signout`;
+    return axiosClient.get(url);
+  },
+  profile(token, id) {
+    const url = `https://binshop.herokuapp.com/api/secret/${id}`;
+    return axios.get(url, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  },
+};
 export default UserAPI;
